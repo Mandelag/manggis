84c842ae0fbfa586354bceb8d523f3475c4f1f04
Keenan Gebze <keenan.gebze@gmail.com>
2018-10-21 02:03:08 +0700

    Add new test cases for the generalized Extractor and Marker class.

--- src/test/java/com/mandelag/manggis/ExtractorTest.java
+++ src/test/java/com/mandelag/manggis/ExtractorTest.java
@@ -1,11 +1,12 @@
 package com.mandelag.manggis;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
-import java.io.BufferedReader;
-import java.io.Reader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.logging.Level;
@@ -18,7 +19,7 @@ import java.util.logging.Logger;
 public class ExtractorTest {
 
     @Test
-    public void extractTextTest() {
+    public void extractTextReaderTest() {
         List<String> extracted = new LinkedList<>();
         ReaderMarker namaExtract = new ReaderMarker("NPSN : ","</li>", str -> extracted.add(str));
         try (Reader r = new BufferedReader(new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("060000.SD"), "UTF8"))) {
@@ -28,6 +29,27 @@ public class ExtractorTest {
             Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
         }
         assertEquals(3476, extracted.size());
+    }
+
+    @Test
+    public void extractReaderTest() throws UnsupportedEncodingException {
+        List<String> extracted = new LinkedList<>();
+        Marker namaExtract = new Marker("NPSN : ".getBytes("UTF-8"),"</li>".getBytes("UTF-8"), bytes -> extracted.add(new String(bytes, Charset.forName("UTF-8"))));
+        List<Marker> markers = new ArrayList<>();
+        markers.add(namaExtract);
+        try (InputStream inputStream = new BufferedInputStream(this.getClass().getClassLoader().getResourceAsStream("060000.SD"))) {
+            Extractor extractor = new Extractor(inputStream, markers);
+            extractor.extract();
+        } catch (IOException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        assertEquals(3476, extracted.size());
+    }
+
+    @Test
+    public void invalidExtractorConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> new Extractor(new ByteArrayInputStream(new byte[256]), null));
+        assertThrows(IllegalArgumentException.class, () -> new Extractor(new ByteArrayInputStream(new byte[256]), new ArrayList<Marker>(10)));
     }
 
 }
