d776b068fb9080152b3f5840567c2cc10d66f70c
Keenan Gebze <keenan.gebze@gmail.com>
2018-10-21 02:02:13 +0700

    Implementing generalized version of Extractor class.

--- src/main/java/com/mandelag/manggis/Extractor.java
+++ src/main/java/com/mandelag/manggis/Extractor.java
@@ -1,30 +1,35 @@
 package com.mandelag.manggis;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * Generalized version of InputStreamExtractor and ReaderExtractor.
  *
  * @author Keenan Mandela Gebze
  * @version 10 October 2018
  */
-public class Extractor<T extends Readable> {
+public class Extractor {
 
-    private final Readable readable;
-    private final Marker[] markers;
+    private final InputStream inputStream;
+    private final List<Marker> markers;
 
-    public Extractor(Readable readable, Marker[] markers) {
-        if (markers == null || markers.length <= 0) {
+    public Extractor(InputStream inputStream, List<Marker> markers) {
+        if (markers == null || markers.isEmpty()) {
             throw new IllegalArgumentException("markers should not be empty.");
         }
-        this.readable = readable;
+        this.inputStream = inputStream;
         this.markers = markers;
     }
 
-    public void extract() {
+    public void extract() throws IOException {
         int read;
-        while ((read = readable.read() ) >= 0) {
-            for (int x=0; x < markers.length; x++ ) {
-                markers[x].listen(read);
-            }
+        while ((read = inputStream.read() ) >= 0) {
+            int tmp = read;
+            markers.forEach((marker) -> {
+                marker.listen(tmp);
+            });
         }
     }
 }
